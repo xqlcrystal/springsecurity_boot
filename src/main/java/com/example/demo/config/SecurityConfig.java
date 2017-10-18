a2113c2377ee7ee740b7ba1115d866b3c0c6ce06
xieql2 <xieql2@lenovo.com>
2017-10-18 20:27:12 +0800

    spring security结合spring -boot

---
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.example.demo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by xieql2 on 2017/10/18.
+ */
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/css/**","/index").permitAll()
+                .antMatchers("/user/**").hasRole("USER")
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .failureUrl("/login-error");
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER");
+
+    }
+}
